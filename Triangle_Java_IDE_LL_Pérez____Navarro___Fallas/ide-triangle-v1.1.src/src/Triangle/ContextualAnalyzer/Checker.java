3f706b73639c43b7defcba42c0e59aab102983cc
magamboatec <magamboa@ic-itcr.ac.cr>
2019-10-12 01:23:36 -0600

    cambios menores
    UnsupportedOperationException en el Checker

--- Triangle_Java_IDE_LL_Pérez____Navarro___Fallas/ide-triangle-v1.1.src/src/Triangle/ContextualAnalyzer/Checker.java
+++ Triangle_Java_IDE_LL_Pérez____Navarro___Fallas/ide-triangle-v1.1.src/src/Triangle/ContextualAnalyzer/Checker.java
@@ -54,6 +54,7 @@ import Triangle.AbstractSyntaxTrees.LetCommand;
 import Triangle.AbstractSyntaxTrees.LetExpression;
 import Triangle.AbstractSyntaxTrees.LoopDoUntilCommand;
 import Triangle.AbstractSyntaxTrees.LoopDoWhileCommand;
+import Triangle.AbstractSyntaxTrees.LoopForCommand;
 import Triangle.AbstractSyntaxTrees.LoopUntilCommand;
 import Triangle.AbstractSyntaxTrees.LoopWhileCommand;
 import Triangle.AbstractSyntaxTrees.MultipleActualParameterSequence;
@@ -960,6 +961,11 @@ public final class Checker implements Visitor {
 
     @Override
     public Object visitLoopDoUntilCommand(LoopDoUntilCommand ast, Object o) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Object visitLoopForCommand(LoopForCommand ast, Object o) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 }
